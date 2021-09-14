673bed65192003d5b31254b840015ec483a81471
jesse <szx961102@gmail.com>
2021-09-14 15:13:26 +0800

    update

--- components/MyList.tsx
+++ components/MyList.tsx
@@ -2,19 +2,19 @@ import Image from "next/image";
 import {Pagination} from "antd";
 import {VodAndDetail} from "../pages";
 
-interface Props{
-    data:VodAndDetail[],
-    page:number,
-    handleChangePage:(v:number)=>void,
-    total:number
+interface Props {
+    data: VodAndDetail[],
+    page: number,
+    handleChangePage: (v: number) => void,
+    total: number
 }
 
-const MyList = ({data,page,handleChangePage,total}:Props) => {
+const MyList = ({data, page, handleChangePage, total}: Props) => {
     return <div>
         <div className={'flex flex-wrap items-center mb-5'}>
-            {data.map(i => <div className={'flex flex-col justify-center items-center mr-2'} key={i.vod_id}>
-                <Image src={i.vod_pic} alt={i.vod_name} width={200} height={300}/>
-                <div>{i.vod_name}</div>
+            {data?.map(i => <div className={'flex flex-col justify-center items-center mr-2'} key={i.vod_id}>
+                <Image src={i?.vod_pic} alt={i.vod_name} width={200} height={300}/>
+                <div>{i?.vod_name}</div>
             </div>)}
         </div>
 
